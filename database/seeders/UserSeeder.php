b60016e67b08bed3f112415a7c382cc835f2eb29
Shawon Sharif <shawon.tappware@gmail.com>
2025-12-25 00:31:44 +0600

    Revert "---"
    This reverts commit 02e3d5b4e7d3eb101b2a2a36ea38b7146329bb5d.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,7 +18,7 @@ class UserSeeder extends Seeder
 
         User::create([
             'name' => 'Test User',
-            'email' => 'testuser@gmail.com',
+            'email' => 'test@gmail.com',
             'password' => Hash::make('12345678'),
         ]);
     }
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,26 +15,16 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <!-- Background Image -->
-        <div class="absolute top-0 left-0 w-full h-full">
-            <img src="https://images.pexels.com/photos/8386440/pexels-photo-8386440.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Body Image" class="w-full h-full object-cover" />
-            <div class="absolute inset-0 bg-black/50"></div> <!-- Optional: Dark overlay for better text contrast -->
-        </div>
-
-        <!-- Main Content -->
-        <div class="relative min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-            <!-- Logo -->
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
 
-            <!-- Content Box -->
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg z-10">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
     </body>
-
 </html>
